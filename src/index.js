d88962b5dfc6c442aa1492493098cfcf3ffba6a9
bettercallazamat <bettercallazamat@gmail.com>
2020-11-06 01:14:52 +0600

    Make addNewTask function independent

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ const main = document.querySelector('main')
 const tasksList = document.querySelector('.tasks-list')
 
 const defaultTasks = () => {
-  // const dtask1 = task('The Winds of Winter');
-  // const dtask2 = task('A Dream of Spring');
-  // const dtask3 = task('A Clash of Kings');
-  // const dtask4 = task('A Game of Thrones');
+  const dtask1 = task('The Winds of Winter');
+  const dtask2 = task('A Dream of Spring');
+  const dtask3 = task('A Clash of Kings');
+  const dtask4 = task('A Game of Thrones');
   const dTodoList1 = toDoList('A Song of Ice and Fire');
   
-  // dTodoList1.list.push(dtask1, dtask2, dtask3, dtask4);
+  dTodoList1.list.push(dtask1, dtask2, dtask3, dtask4);
 
   return dTodoList1;
 };
@@ -32,16 +32,22 @@ const render = (obj) => {
   }
 }
 
+const addNewTask = (obj) => {
+  const newTaskInput = document.getElementById("task-input").value;
+  const newTask = task(newTaskInput);
+  obj.list.push(newTask);
+};
 
 const defaultInputs = defaultTasks();
 console.log(defaultInputs);
-// render(defaultInputs);
+render(defaultInputs);
 
 
-// submit.addEventListener('click', (e) => {
-//   e.preventDefault();
-//   defaultInputs.add();
-// });
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  addNewTask(defaultInputs);
+  render(defaultInputs);
+});
 
 
 
--- src/tabs/todo.js
+++ src/tabs/todo.js
@@ -1,20 +1,14 @@
-import { render } from "../index";
-
 const submit = document.getElementById("submit");
 const form = document.getElementById("book-form");
 
 const main = document.querySelector("main");
 
 const toDoList = (title, list = []) => {
-  // const add = (obj) => {
-  //   const newTaskInput = document.getElementById("task-input").value;
-  //   const newTask = task(newTaskInput);
-  //   this.list.push(newTask);
-  // };
-
   return { title, list };
 };
 
 const task = (title) => {
   return { title };
 };
+
+export { toDoList, task };
