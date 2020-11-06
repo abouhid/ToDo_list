9a0e630509b8c5bae9bebb6ef6661e9c1c269f44
bettercallazamat <bettercallazamat@gmail.com>
2020-11-06 22:30:03 +0600

    Add new inputs and showBtn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-import {
-  toDoList,
-  task
-} from './tabs/todo'
+import { toDoList, task } from './tabs/todo'
 
 const submit = document.getElementById('submit');
 const sidebar_submit = document.getElementById('sidebar-submit');
@@ -11,28 +8,23 @@ const tasksList = document.querySelector('.tasks-list')
 const projectsListContainer = document.querySelector('.project-names')
 const form = document.getElementById("book-form");
 const sidebar_form = document.getElementById("sidebar-form");
-
 const projectsList = []
 
-
 const defaultTasks = () => {
-  const dtask1 = task('The Winds of Winter');
-  const dtask2 = task('A Dream of Spring');
-  const dtask3 = task('A Clash of Kings');
-  const dtask4 = task('A Game of Thrones');
+  const dtask1 = task('The Winds of Winter', 'desc1', '2020-12-12', 'high');
+  const dtask2 = task('A Dream of Spring', 'desc1', '2020-12-12', 'high');
+  const dtask3 = task('A Clash of Kings', 'desc1', '2020-12-12', 'high');
+  const dtask4 = task('A Game of Thrones', 'desc1', '2020-12-12', 'high');
   const dTodoList1 = toDoList('A Song of Ice and Fire');
   const dTodoList2 = toDoList('The Lord of the Rings');
-  const dtask5 = task('The Two Towers');
+  const dtask5 = task('The Two Towers', 'desc1', '2020-12-12', 'high');
+  
+  
+  dTodoList1.list.push(dtask1, dtask2, dtask3, dtask4);
   dTodoList2.list.push(dtask5);
 
-
-  dTodoList1.list.push(dtask1, dtask2, dtask3, dtask4);
-  projectsList.unshift(dTodoList1);
-  projectsList.unshift(dTodoList2);
-
-  console.log(projectsList);
-
-  return dTodoList1;
+  projectsList.push(dTodoList1);
+  projectsList.push(dTodoList2);
 };
 
 const renderTasks = (obj) => {
@@ -43,18 +35,53 @@ const renderTasks = (obj) => {
   for (let i = 0; i < obj.list.length; i += 1) {
     const taskContainer = document.createElement('div');
     const title = document.createElement('h3');
+    const detailsContainer = document.createElement("div");
+    const desc = document.createElement("p");
+    const date = document.createElement("p");
+    const priority = document.createElement("p");
+    const showBtn = addShowDetailsBtn(detailsContainer);
+  
     title.textContent = obj.list[i].title;
+    desc.textContent = obj.list[i].desc;
+    date.textContent = obj.list[i].dueDate;
+    priority.textContent = obj.list[i].priority;
+
+    detailsContainer.style.display = "none";
+
     taskContainer.appendChild(title);
+    taskContainer.appendChild(showBtn);
+    detailsContainer.appendChild(desc);
+    detailsContainer.appendChild(priority);
+    detailsContainer.appendChild(date);
+    taskContainer.appendChild(detailsContainer);
     tasksList.appendChild(taskContainer);
   }
 }
 
 const addNewTask = (obj) => {
-  const newTaskInput = document.getElementById("task-input").value;
-  const newTask = task(newTaskInput);
+  const taskTitleInput = document.getElementById("task-input").value;
+  const taskDescInput = document.getElementById("task-desc-input").value;
+  const taskDateInput = document.getElementById("task-date-input").value;
+  const taskPriorityInput = document.getElementById("task-priority-input").value;
+  
+  const newTask = task(taskTitleInput, taskDescInput, taskDateInput, taskPriorityInput);
+  console.log(newTask);
   form.reset();
   obj.list.unshift(newTask);
 };
+
+const addShowDetailsBtn = (container) => {
+  const chevron = document.createElement('span');
+  chevron.textContent = "⌄";
+
+  chevron.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (container.style.display === "block") container.style.display = "none";
+    else container.style.display = "block";
+  });
+
+  return chevron;
+}
 
 submit.addEventListener('click', (e) => {
   e.preventDefault();
@@ -104,9 +131,6 @@ sidebar_submit.addEventListener('click', (e) => {
 });
 
 
-
-
-const defaultInputs = defaultTasks();
+defaultTasks();
 renderLists(projectsList);
-
-renderTasks(defaultInputs);
+renderTasks(projectsList[0]);
--- src/tabs/todo.js
+++ src/tabs/todo.js
@@ -2,8 +2,8 @@ const toDoList = (title, list = []) => {
   return { title, list };
 };
 
-const task = (title) => {
-  return { title };
+const task = (title, desc, priority, dueDate) => {
+  return { title, desc, priority, dueDate };
 };
 
 export { toDoList, task };
